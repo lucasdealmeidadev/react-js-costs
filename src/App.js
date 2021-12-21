c52fc256a192df8a346161db0bb6dbbf1751f06d
Lucas de Almeida <lucasdealmeida.monteiro@gmail.com>
2021-12-20 21:37:18 -0300

    Estruturando o projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Company, NewProject, Contact } from './components/pages';
+import { Container } from './layout';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<ul>
+				<li>Home</li>
+				<li>Contato</li>
+			</ul>
+			<Container customClass='min-height'>
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+					<Route path='/company' element={<Company />} />
+					<Route path='/new-project' element={<NewProject />} />
+					<Route path='/contact' element={<Contact />} />
+				</Routes>
+			</Container>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/pages/index.js
@@ -0,0 +1,11 @@
+import Home from './Home';
+import Company from './Company';
+import NewProject from './NewProject';
+import Contact from './Contact';
+
+export {
+    Home,
+    Company,
+    NewProject,
+    Contact
+}
