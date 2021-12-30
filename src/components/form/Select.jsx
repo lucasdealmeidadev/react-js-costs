655eee66ea0b3e8734bd9a96d6aa8b1991e1c89f
Lucas de Almeida <lucasdealmeida.monteiro@gmail.com>
2021-12-29 21:51:30 -0300

    Conectando com API pelo React

--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -2,12 +2,15 @@ import { memo } from 'react';
 
 import './Select.css';
 
-function Select({ text, name, options, value, handleOnChange}) {
+function Select({ text, name, options, value, handleOnChange }) {
     return (
         <div className='form-control'>
             <label htmlFor={name}>{text}</label>
             <select name={name} id={name}>
                 <option>Selecione uma opção</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}>{option.name}</option>
+                ))}
             </select>
         </div>
     );
--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,9 +1,25 @@
-import { memo } from 'react';
+import { useState, useEffect, memo } from 'react';
 import { Input, Select, SubmitButton } from '../form';
 
 import './ProjectForm.css';
 
 function ProjectForm({ btnText }) {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data);
+        })
+        .catch((error) => console.log(error));
+    }, []);
+
     return (
         <form className='form'>
             <Input
@@ -21,8 +37,9 @@ function ProjectForm({ btnText }) {
             <Select
                 name='category_id'
                 text='Selecione a categoria'
+                options={categories}
             />
-            <SubmitButton text={btnText}/>
+            <SubmitButton text={btnText} />
         </form>
     );
 }
